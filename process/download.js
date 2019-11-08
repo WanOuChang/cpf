8d45e2c9a80f38415ec3b36653788181859bf98e
WanOuChang <847014574@qq.com>
2019-11-08 19:41:50 +0800

    下载项目

--- process/download.js
+++ process/download.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 const download = require('download-git-repo');
 
 const { exec } = require('child_process');
@@ -11,3 +13,10 @@ download(`github:WanOuChang/${param}-demo`, 'test', (error) => {
     exec('npm install');
     console.log('成功')
 })
+
+// download(`github:WanOuChang/vue-demo`, 'test', (error) => {
+//     if (error) throw error;
+//     process.chdir(`./test/vue-demo`)
+//     exec('npm install');
+//     console.log('成功')
+// })
